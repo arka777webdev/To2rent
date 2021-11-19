48538516fa3b5816d8323109a501501bab247d73
arka777webdev <arka7772002@mail.ru>
2021-11-19 13:35:08 +0300

    project init

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.slider').slick({
+
+
+  prevArrow:'<button type="button" class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt="prev"></button>',
+  nextArrow:'<button type="button" class="slick-arrow slick-next"><img src="../images/arrow-right.svg" alt="next"></button>',
+
+
+  });
+});
+
+
