33d40be659a7498d109267c4e17c7752c3a56297
arka777webdev <arka7772002@mail.ru>
2021-11-20 09:58:06 +0300

    files modified

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(function () {
   $('.slider').slick({
 
 
-  prevArrow:'<button type="button" class="slick-arrow slick-prev"><img src="../images/arrow-left.svg" alt="prev"></button>',
-  nextArrow:'<button type="button" class="slick-arrow slick-next"><img src="../images/arrow-right.svg" alt="next"></button>',
+  prevArrow:'<button type="button" class="slick-arrow slick-prev"><img src="images/arrow-left.svg" alt="prev"></button>',
+  nextArrow:'<button type="button" class="slick-arrow slick-next"><img src="images/arrow-right.svg" alt="next"></button>',
 
 
   });
